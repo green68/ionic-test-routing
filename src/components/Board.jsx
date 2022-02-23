2a927a4ebfd0f84e38df626d5f6d5466568af78b
green68 <eric.vercheval@hotmail.fr>
2022-02-23 01:55:07 +0100

    add css and actions

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,6 +2,7 @@ import { IonButton } from "@ionic/react"
 import { getUser, removeUser } from "../utils/datas"
 import {useHistory} from "react-router-dom"
 import { useState } from "react"
+import "./Board.css"
 
 const Board = () => {
 
@@ -14,11 +15,16 @@ const Board = () => {
         removeUser()
     }
 
+    const actions = [1,2,3,4,5]
+    const listActions = actions.map( action => <IonButton key={action}>action {action}</IonButton>)
     return (
-        <>
+        <div className="board-container">
             <p>Tableau de bord de {user?.username}</p>
+            <div className="board-actions">
+                {listActions}
+            </div>
             <IonButton color="danger" onClick={handleDeconnexion}>Déconnexion</IonButton>
-        </>
+        </div>
     )
 }
 
